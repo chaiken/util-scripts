748e97753ff103f4d3c402026dfad5a495e335ac
Alison Chaiken <alison@she-devel.com>
2023-04-08 09:07:36 -0700

    endian-cpp: make more portable
    In the spirit of endian-type discovery, don't assume that the host is
    little-endian.  Add a few comments about aligned_storage and
    placement-new.

--- endian-cpp.cc
+++ endian-cpp.cc
@@ -12,18 +12,36 @@ bool is_little_endian(const int val, const char byteval) {
   return (byteval == bytes[0]);
 }
 
-bool is_little_endian2(int val, const char byteval) {
-  std::aligned_storage_t<INTSIZE, alignof(char)> byte_store;
+bool is_little_endian2(const unsigned val, const char byteval) {
+  // A stack allocation unlike std::aligned_alloc.
+  std::aligned_storage_t<INTSIZE, alignof(unsigned)> byte_store{};
+  // Because of the "placement new", bytes on the stack.
   char *bytes = new (&byte_store) char;
+  // A simple assignment works because bytes is properly aligned and sized.
   *bytes = val;
   return (byteval == bytes[0]);
 }
 
+template <std::size_t HOST_INTSIZE> bool is_big_endian(const unsigned val) {
+  switch (HOST_INTSIZE) {
+  case 2U:
+    return (htobe16(val) == val);
+  case 4U:
+    return (htobe32(val) == val);
+  case 8U:
+    return (htobe64(val) == val);
+  default:
+    std::cerr << "Unknown architecture";
+    return false;
+  }
+}
+
 int main() {
-  int container = 1;
+  unsigned container = htole64(1);
   assert(true == is_little_endian(container, 1));
-  int container_be = htobe32(container);
+  unsigned container_be = htobe64(container);
   assert(false == is_little_endian(container_be, 1));
+  assert(true == is_big_endian<INTSIZE>(container_be));
 
   assert(true == is_little_endian2(container, 1));
   assert(false == is_little_endian2(container_be, 1));
